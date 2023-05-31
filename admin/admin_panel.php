a834f53e16ca135ca16dab50bd125a8e2a3cda93
Haithemkoriche <88111725+Haithemkoriche@users.noreply.github.com>
2023-05-31 01:12:12 +0100

    admin panel

--- admin/admin_panel.php
+++ admin/admin_panel.php
@@ -1,53 +1,129 @@
 <?php
-session_start();
+include("../config/bdd.php");
+// Fetch counts from the database
+$query = "SELECT COUNT(*) AS count FROM participants";
+$result = $conn->query($query);
+$row = $result->fetch_assoc();
+$participantsCount = $row['count'];
 
-// Check if admin is logged in
-if (!isset($_SESSION["admin_email"])) {
-  header("Location: login.php");
-  exit();
-}
+$query = "SELECT COUNT(*) AS count FROM formateurs";
+$result = $conn->query($query);
+$row = $result->fetch_assoc();
+$formateursCount = $row['count'];
 
-// Retrieve the admin email from the session
-$adminEmail = $_SESSION["admin_email"];
+$query = "SELECT COUNT(*) AS count FROM activities";
+$result = $conn->query($query);
+$row = $result->fetch_assoc();
+$activitiesCount = $row['count'];
 
-// Display welcome message or perform admin-specific actions
-// ...
+$query = "SELECT COUNT(*) AS count FROM ateliers";
+$result = $conn->query($query);
+$row = $result->fetch_assoc();
+$ateliersCount = $row['count'];
 
-// Logout functionality
-if (isset($_GET["logout"])) {
-  session_destroy();
-  header("Location: login.php");
-  exit();
-}
+$query = "SELECT COUNT(*) AS count FROM evenements";
+$result = $conn->query($query);
+$row = $result->fetch_assoc();
+$evenementsCount = $row['count'];
+
+$query = "SELECT COUNT(*) AS count FROM animateurs";
+$result = $conn->query($query);
+$row = $result->fetch_assoc();
+$animateursCount = $row['count'];
+
+$query = "SELECT COUNT(*) AS count FROM contacts";
+$result = $conn->query($query);
+$row = $result->fetch_assoc();
+$contactsCount = $row['count'];
+
+$query = "SELECT COUNT(*) AS count FROM carousels";
+$result = $conn->query($query);
+$row = $result->fetch_assoc();
+$carouselsCount = $row['count'];
+
+// Close the database connection
+$conn->close();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Admin Panel</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-<body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-    <a class="navbar-brand" href="#">Admin Panel</a>
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <span class="nav-link">Welcome, <?php echo $adminEmail; ?></span>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="?logout">Logout</a>
-      </li>
-    </ul>
-    </div>
-  </nav>
 
+<?php include('layout.php'); ?>
+
+
+<div class="container mt-5">
   <div class="container mt-5">
-    <h1>Welcome to the Admin Panel</h1>
-    <!-- Admin-specific actions and content -->
-    <!-- ... -->
+    <div class="row gap-sm-3 gap-lg-0">
+      <div class="col-md-12 col-lg-3">
+        <div class="card bg-primary text-white">
+          <div class="card-body">
+            <h5 class="card-title"> <i class="fas fa-users"></i> Participantes</h5>
+            <h2 class="card-text d-flex justify-content-between"> <?php echo $participantsCount; ?> <a href="" class="text-white "><i class="fas fa-arrow-right"></i></a></h2>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-12 col-lg-3">
+        <div class="card bg-success text-white">
+          <div class="card-body">
+            <h5 class="card-title"><i class="fas fa-users"></i> Formateurs</h5>
+            <h2 class="card-text d-flex justify-content-between"><?php echo $formateursCount; ?><a href="" class="text-white "><i class="fas fa-arrow-right"></i></a></h2>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-12 col-lg-3">
+        <div class="card bg-info text-white">
+          <div class="card-body">
+            <h5 class="card-title"><i class="fas fa-list"></i> Activités</h5>
+            <h2 class="card-text d-flex justify-content-between"><?php echo $activitiesCount; ?> <a href="" class="text-white "><i class="fas fa-arrow-right"></i></a></h2>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-12 col-lg-3">
+        <div class="card bg-warning text-dark">
+          <div class="card-body">
+            <h5 class="card-title"><i class="fas fa-tools"></i> Ateliers</h5>
+            <h2 class="card-text d-flex justify-content-between"><?php echo $ateliersCount; ?> <a href="" class="text-white "><i class="fas fa-arrow-right"></i></a></h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row mt-4">
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title"> <i class="fas fa-calendar-alt"></i> Événements </h5>
+            <h2 class="card-text d-flex justify-content-between"><?php echo $evenementsCount; ?> <a href="" class=""><i class="fas fa-arrow-right"></i></a></h2>
+          </div>
+        </div>
+        <div class="card mt-4">
+          <div class="card-body">
+            <h5 class="card-title"><i class="fas fa-envelope animated faa-shake"></i>
+ Messages </h5>
+            <h2 class="card-text d-flex justify-content-between"><?php echo $contactsCount; ?><a href="" class=""><i class="fas fa-arrow-right"></i></a></h2>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+      <div class="card">
+          <div class="card-body">
+            <h5 class="card-title"><i class="fas fa-user"></i> Animateurs </h5>
+            <h2 class="card-text d-flex justify-content-between"><?php echo $animateursCount; ?><a href="" class=""><i class="fas fa-arrow-right"></i></a></h2>
+          </div>
+        </div>
+        <div class="card mt-4">
+          <div class="card-body">
+            <h5 class="card-title"><i class="fas fa-play"></i> Carousels </h5>
+            <h2 class="card-text d-flex justify-content-between"><?php echo $carouselsCount; ?><a href="" class=""><i class="fas fa-arrow-right"></i></a></h2>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-</body>
-</html>
+  <div class="toast align-items-center text-bg-primary border-0" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="d-flex">
+      <div class="toast-body">
+        Hello, world! This is a toast message.
+      </div>
+      <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+  </div>
+
+  <?php include('footer.html'); ?>
---
+++ admin/layout.php
@@ -0,0 +1,62 @@
+<?php
+session_start();
+
+// Check if admin is logged in
+if (!isset($_SESSION["admin_email"])) {
+  header("Location: index.html");
+  exit();
+}
+
+// Retrieve the admin email from the session
+$adminEmail = $_SESSION["admin_email"];
+
+// Display welcome message or perform admin-specific actions
+// ...
+
+// Logout functionality
+if (isset($_GET["logout"])) {
+  session_destroy();
+  header("Location: index.html");
+  exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Admin Panel</title>
+    <link rel="stylesheet" href="../assets/fonts/css/all.css" />
+    <link rel="stylesheet" href="../assets/fonts/css/fontawesome.css" />
+    <link rel="stylesheet" href="../assets/fonts/css/all.min.css" />
+    <link rel="stylesheet" href="../assets/fonts/css/fontawesome.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome-animation/0.3.0/font-awesome-animation.min.css" />
+
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+    />
+    
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">Admin Panel</a>
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <span class="nav-link"
+              >Welcome,
+              <?php echo $adminEmail; ?></span
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="?logout">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container mt-5"></div>
+  </body>
+</html>
